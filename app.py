1c38860990b0ee397ab43c9f1610add7dae0797f
Roni Bärman <104189902+roni-b@users.noreply.github.com>
2022-09-25 15:58:47 +0300

    Change app.py to use .env secret key

--- app.py
+++ app.py
@@ -3,7 +3,5 @@ from os import getenv
 
 app = Flask(__name__)
 
-#app.secret_key = getenv("SECRET_KEY")
-app.secret_key = "28c3d325dddb0c6b63069c300aece32f"
-from db import db
-import routes
+app.secret_key = getenv("SECRET_KEY")
+
