701e5998421361027625c41719f7f5bd16afad9e
Roni Bärman <104189902+roni-b@users.noreply.github.com>
2022-09-27 15:57:56 +0300

    Add psql schema

---
+++ schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    password TEXT,
+    username TEXT UNIQUE,
+    admin TEXT
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    sent_at TIMESTAMP,
+    user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    user_id REFERENCES users
+);
+
+CREATE TABLE groupMembers (
+    group_id INTEGER REFERENCES groups,
+    member_id INTEGER REFERENCES members
+);
