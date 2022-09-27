6b9edb813b62ba65d5098506e969e1c4a7087ad2
Roni Bärman <104189902+roni-b@users.noreply.github.com>
2022-09-27 14:34:54 +0300

    Fix message datetime for Heroku

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ from db import db
 import routes
 from flask import session, request
 import users
+from datetime import datetime, timezone, timedelta
 
 def get_messages():
     sql = ("SELECT content, sent_at FROM messages;")
@@ -14,8 +15,10 @@ def add_message(new):
             return False
         user_id = users.user_id()
         if len(new) > 0:
-            sql = "INSERT INTO messages (content, sent_at, user_id) VALUES (:new_message, NOW(), :user_id)"
-            db.session.execute(sql, {"new_message":new, "user_id":user_id})
+            utc3 = timedelta(hours=3)
+            time = datetime.now(timezone(utc3))
+            sql = "INSERT INTO messages (content, sent_at, user_id) VALUES (:new_message, :sent_at, :user_id)"
+            db.session.execute(sql, {"new_message":new, "sent_at":time, "user_id":user_id})
             db.session.commit()
             return True
         return False
