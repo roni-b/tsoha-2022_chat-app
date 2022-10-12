ccdecfe2f6847693e33660a6daf82ee12c3f66b8
Roni Bärman <104189902+roni-b@users.noreply.github.com>
2022-10-12 17:27:25 +0300

    Add new tables for new content

--- schema.sql
+++ schema.sql
@@ -23,3 +23,22 @@ CREATE TABLE groupMembers (
     member_id INTEGER REFERENCES users
 );
 
+CREATE TABLE messageRatings (
+    id SERIAL PRIMARY KEY,
+    message_id INTEGER REFERENCES messages,
+    likes INTEGER DEFAULT 0,
+    dislikes INTEGER DEFAULT 0
+);
+
+CREATE TABLE publicMessages (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    sent_at TIMESTAMP
+);
+
+CREATE TABLE reports (
+    id SERIAL PRIMARY KEY,
+    message_id INTEGER REFERENCES messages,
+    report_count INTEGER DEFAULT 0
+);
+
