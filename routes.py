41a6c424a8177103045e722d931ccef5c73a3df7
Roni Bärman <104189902+roni-b@users.noreply.github.com>
2022-10-23 20:57:18 +0300

    Increase message max length

--- routes.py
+++ routes.py
@@ -48,7 +48,7 @@ def logout():
 @app.route("/send", methods=["POST"])
 def send():
     new = request.form["new_message"]
-    if len(new) > 200:
+    if len(new) > 1000:
         return render_template("error.html", error="Viesti on liian pitkä")
     if len(new) == 0:
         return render_template("error.html", error="Viesti ei voi olla tyhjä")
@@ -84,7 +84,7 @@ def messages():
         username = request.args["username"]
         if username != session["username"]:
             return redirect("/")
-        if len(query) > 0 and len(query) < 200:
+        if len(query) > 0 and len(query) < 1000:
             functions.edit_message(id, query)
     except:
         try:
